16b023e060acdf39d51f97b1fbe742fed53c9c7a
unknown <youssefabidi929@gmail.com>
2024-07-23 17:33:41 +0100

    add register with security

--- book-network/src/main/java/com/abidiyoussef/book/BookNetworkApiApplication.java
+++ book-network/src/main/java/com/abidiyoussef/book/BookNetworkApiApplication.java
@@ -1,13 +1,26 @@
 package com.abidiyoussef.book;
 
+import com.abidiyoussef.book.role.Role;
+import com.abidiyoussef.book.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookNetworkApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookNetworkApiApplication.class, args);
 	}
-
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
+	}
 }
