ea53c93af7ad5bfdad10609f334c08645e531937
ABIDI Youssef <youssef.abidi@viseo.com>
2024-07-24 17:08:19 +0100

    add-global-exception-handling

---
+++ book-network/src/main/java/com/abidiyoussef/book/exception/ActivationTokenException.java
@@ -0,0 +1,8 @@
+package com.abidiyoussef.book.exception;
+
+
+public class ActivationTokenException extends RuntimeException {
+    public ActivationTokenException(String message) {
+        super(message);
+    }
+}
