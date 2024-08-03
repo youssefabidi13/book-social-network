445714abdbb8e3a3b69b21a6e623dcdc3b234b9e
unknown <youssefabidi929@gmail.com>
2024-08-03 12:18:30 +0100

    implement-registration-page

--- book-network-ui/src/app/app.module.ts
+++ book-network-ui/src/app/app.module.ts
@@ -4,16 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { LoginComponent } from './pages/login/login.component';
+import {FormsModule} from "@angular/forms";
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule
+    ],
   providers: [HttpClient],
   bootstrap: [AppComponent]
 })
