4e28511ca2120b447bac5c8b9641e134e75aad5d
unknown <youssefabidi929@gmail.com>
2024-08-08 14:17:50 +0100

    add return-book component

--- book-network-ui/src/app/modules/book/book.module.ts
+++ book-network-ui/src/app/modules/book/book.module.ts
@@ -11,6 +11,7 @@ import { MyBooksComponent } from './pages/my-books/my-books.component';
 import { ManageBookComponent } from './pages/manage-book/manage-book.component';
 import {FormsModule} from "@angular/forms";
 import { BorrowedBookListComponent } from './pages/borrowed-book-list/borrowed-book-list.component';
+import { ReturnBooksComponent } from './pages/return-books/return-books.component';
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import { BorrowedBookListComponent } from './pages/borrowed-book-list/borrowed-b
     RatingComponent,
     MyBooksComponent,
     ManageBookComponent,
-    BorrowedBookListComponent
+    BorrowedBookListComponent,
+    ReturnBooksComponent
   ],
   imports: [
     CommonModule,
